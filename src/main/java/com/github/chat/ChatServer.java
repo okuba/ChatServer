ce1dbb890f3ec7d8adc7e0b43cbca3caac65aae3
okuba <yousuke5235@gmail.com>
2015-07-31 13:42:04 +0900

    refs #3 listenメソッドの追加
    import文の追加、listenメソッドの追加、mainからの呼出しの追加を行った。

--- src/main/java/com/github/chat/ChatServer.java
+++ src/main/java/com/github/chat/ChatServer.java
@@ -1,10 +1,55 @@
 package com.github.chat;
 
+import java.io.*;
+import java.net.*;
+import java.util.*;
+
+
 public class ChatServer
 {
+    private ServerSocket server;
+    
+    public void listen()
+    {
+        Socket socket = null;
+        
+        try
+        {
+            server = new ServerSocket(18080);
+            System.out.println("Server started ... ...");
+            
+            while(true)
+            {
+                socket = server.accept();
+                System.out.println("new connect ... ...");
+            }
+        }
+        catch(IOException anException)
+        {
+            anException.printStackTrace();
+        }
+        finally
+        {
+            if(socket != null)
+            {
+                try
+                {
+                    socket.close();
+                }
+                catch(IOException anException)
+                {
+                    anException.printStackTrace();
+                }
+            }
+        }
+    }
+    
+    
+    
 	public static void main(String[] arguments)
 	{
         ChatServer chat = new ChatServer();
+        chat.listen();
 	}
 }
 
